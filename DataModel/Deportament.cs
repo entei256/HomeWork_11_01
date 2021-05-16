027b4209bcc871bcf967971bb358c43b452f8b7e
Игорь <crazydead256@gmail.com>
2021-05-17 01:40:03 +0300

    Заготовки моделей

---
+++ DataModel/Deportament.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace WpfApp1.DataModel
+{
+    class Deportament
+    {
+        private static uint id;            //Счетчик ID
+        public uint ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public ObservableCollection<Deportament> Deportaments { get; set; }      //Коллекция дочерних депортаментов. 
+        public ObservableCollection<Staff> Staffs { get; set; }                  //Коллекция сотруднико. TO-DO: переопределить тип после создании структуры сотрудников.
+        public int CountStaff 
+        { 
+            get 
+            { 
+                if (Staffs == null)
+                    return 0;
+                return Staffs.Count;                
+            } 
+        }                        //Число сотрудников
+
+        public Deportament()
+        {
+            id++;                      //увеличиваем счетчик.
+            this.ID = id;               //Записываем ID текущего экземпляра.
+            this.DateTime = DateTime.Now;  //При создании экземпляра, добовляем текущую дату.
+            Deportaments = new ObservableCollection<Deportament>();
+            Staffs = new ObservableCollection<Staff>();
+        }
+    }
+}
---
+++ DataModel/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.DataModel
+{
+    interface Staff
+    {
+        private static uint id = 0;
+        public uint ID { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+    }
+}
